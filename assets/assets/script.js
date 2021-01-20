1d079274695f9a21486327429ccf34c1c7b1ee8b
Sophia Mazurek <sophiamazurek@Sophias-MacBook-Pro-2.local>
2021-01-20 09:25:35 -0600

    added current day but trying to get it to connect with HTML

--- assets/assets/script.js
+++ assets/assets/script.js
@@ -1,4 +1,19 @@
-//Current day and time
+//current date and time 
+//refrence:https://www.w3resource.com/javascript-exercises/javascript-date-exercise-2.php
+var curday = function (sp){
+    today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth()+1;
+    var yyyy = today.getFullYear();
+
+    if (dd<10) dd='0'+dd;
+    if (mm<10) mm='0'+mm;
+    return (mm+sp+dd+sp+yyyy);
+     
+};
+console.log(curday('-'));
+
+//Time Array 
 var timeArray=['8','9','10','11','12','13','14','15','16','17'];
 var currentTime=moment().format("HH");  //double check current time is in military
 console.log(currentTime)
